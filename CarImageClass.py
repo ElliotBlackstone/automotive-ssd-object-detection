a625f945e35003e5ebd8af249ad300e9749e478f
ElliotBlackstone <eblackst2@gmail.com>
2025-10-06 23:07:52 -0400

    updates

--- CarImageClass.py
+++ CarImageClass.py
@@ -83,16 +83,14 @@ class ImageClass(Dataset):
                 boxes[i, j] = img_df.iloc[i][coord]
                 j = j + 1
 
-            areas[i] = (boxes[i,2] - boxes[i,0]).clamp(min=0) * (boxes[i,3] - boxes[i,1]).clamp(min=0)
+            # areas[i] = (boxes[i,2] - boxes[i,0]).clamp(min=0) * (boxes[i,3] - boxes[i,1]).clamp(min=0)
 
         iscrowd = torch.zeros(len(img_df), dtype=torch.int64)
 
         # Wrap into tv_tensors so transforms know how to move boxes with the image
-        # w, h = img.size
-        # w, h = img.shape[1:]
-        # img = tv_tensors.Image(img)
+        _, H, W = img.shape
         boxes = tv_tensors.BoundingBoxes(
-            boxes, format="XYXY", canvas_size=img.shape[-2:]
+            boxes, format="XYXY", canvas_size=(H, W)
         )
 
         target = {
@@ -106,7 +104,7 @@ class ImageClass(Dataset):
         if self.transform is not None:
             img, target = self.transform(img, target)
 
-        target['areas'] = (target['boxes'][:,2] - target['boxes'][:,0]).clamp(0, img.shape[-1]) * (target['boxes'][:,3] - target['boxes'][:,1]).clamp(0, img.shape[-2])
+        target['areas'] = (target['boxes'][:,2] - target['boxes'][:,0]).clamp(0, W) * (target['boxes'][:,3] - target['boxes'][:,1]).clamp(0, H)
 
         # Convert back to plain tensors for model consumption
         # target["boxes"] = torch.as_tensor(target["boxes"], dtype=torch.float32)
@@ -114,10 +112,16 @@ class ImageClass(Dataset):
 
         return img, target # {**{"file_name": img_name}, **{"target": obj_dict}}
     
-
-    def show_with_box(self, index: int, color: str = "C0", lw: int = 2, label: bool = False) -> Tuple[Figure, Axes]:
+    
+    def show_with_box(self,
+                      index: int,
+                      color: str = "C0",
+                      lw: int = 2,
+                      label: bool = False,
+                      pred_box: bool = False,
+                      bbox_pred: torch.Tensor = torch.zeros((1,4))
+                      ) -> Figure:
         
-
         # convert the image to a numpy array
         img, target = self[index]
 
@@ -140,7 +144,6 @@ class ImageClass(Dataset):
         elif arr.dtype != np.uint8:
             arr = arr.astype(np.uint8)
         
-
 
         # plot image
         H, W = arr.shape[:2]
@@ -172,7 +175,7 @@ class ImageClass(Dataset):
             )
             ax.add_patch(rect)
             if label:
-                im_label_masked = self[index][1]['labels'][i]
+                im_label_masked = target['labels'][i]
                 im_label = list(self.class_to_idx.keys())[list(self.class_to_idx.values()).index(im_label_masked)]
                 ax.text(
                     x_min, y_min,
@@ -184,9 +187,33 @@ class ImageClass(Dataset):
                     bbox=dict(facecolor=color, alpha=0.6, pad=2, edgecolor="none"),
                 )
         
+        if pred_box == True:
+            for i in range(len(bbox_pred)):
+                x_min = bbox_pred[i, 0]
+                y_min = bbox_pred[i, 1]
+                x_max = bbox_pred[i, 2]
+                y_max = bbox_pred[i, 3]
+                x_min, y_min = max(0.0, x_min), max(0.0, y_min)
+                x_max, y_max = min(W - 1, x_max), min(H - 1, y_max)
+                if not (x_max > x_min and y_max > y_min):
+                    # raise ValueError("Degenerate or inverted box after clipping.")
+                    continue
+
+                
+                rect = Rectangle(
+                    (x_min, y_min),
+                    x_max - x_min,
+                    y_max - y_min,
+                    linewidth=lw,
+                    edgecolor='g',
+                    facecolor="none",
+                )
+                ax.add_patch(rect)
+        
         ax.axis("off")
 
-        return fig, ax
+        plt.close(fig)
+        return fig
 
 
     
