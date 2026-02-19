b6ca617fd46e23c876246b57e510db4ef42cd21e
ElliotBlackstone <eblackst2@gmail.com>
2026-02-19 13:46:19 -0500

    updates

--- SSD_int8_realtime_video.py
+++ SSD_int8_realtime_video.py
@@ -9,7 +9,11 @@ from SSDInt8_ONNX_Pred import SSDInt8ONNXPredictor, PreprocessConfig
 
 
 # to run:
+# desktop
 # python SSD_int8_realtime_video.py --model C:\Users\eblac\Documents\GitHub\self-driving-car\PTQ_testing\ssd_int8.onnx --show-fps
+
+# laptop
+# python SSD_int8_realtime_video.py --model C:\Users\eblac\OneDrive\Documents\GitHub\self-driving-car\PTQ_testing\ssd_int8_with_pre_post.onnx --show-fps
 
 
 def build_class_names_fg(class_to_idx: dict) -> list[str]:
